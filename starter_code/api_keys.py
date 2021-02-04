08129532f4ae2f58b29682a8c732e43e0a2472a8
marlyng <marlyngmorales26@gmail.com>
2021-02-04 17:26:44 -0500

    updated

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "6b925047ce005a37840c36a4ecf18011"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCJkAykuxabaWNIkhJfA3PEVrYujLmF2DM"
+g_key = ""
